f76e0ab472ff9ee7acd6ab929dfb0020187b49e1
pooja.wali <pooja.wali@ZIPLBLRSY102.zycus.net>
2018-03-08 15:45:36 +0530

    Adding edit request basic

---
+++ Object Repository/Login credentials/Password xpath.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Password xpath</name>
+   <tag></tag>
+   <elementGuidId>4feaf888-e863-4f95-a974-0e4501233893</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id='div1']/input</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Login credentials/Username xpath.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Username xpath</name>
+   <tag></tag>
+   <elementGuidId>93538787-a891-4b24-ac47-c746d3d7ca4a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id='emailAddressGhost']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
