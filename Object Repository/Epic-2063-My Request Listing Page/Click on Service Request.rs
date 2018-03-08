1fc4c7226ce328e19b596e5c46a03e914a1ab979
priyanka.n <priyanka.n@ZIPLBLRSY118.zycus.net>
2018-03-08 18:46:57 +0530

    My Request Listing Page-TC-1

---
+++ Object Repository/Epic-2063-My Request Listing Page/Click on Service Request.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Click on Service Request</name>
+   <tag></tag>
+   <elementGuidId>4116b4f1-f18c-4522-a1bb-7259dd5b279b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>html/body/dew-app-root/dew-irequest-layout/irequest-header/dew-row[2]/dew-col[2]/dew-flex/dew-flex-item[3]/div/div/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
