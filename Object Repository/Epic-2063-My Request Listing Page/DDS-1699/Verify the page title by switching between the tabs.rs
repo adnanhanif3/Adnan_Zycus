fe9eb413da17bc5de71f99c606e6ca4db571a36b
priyanka.n <priyanka.n@ZIPLBLRSY118.zycus.net>
2018-03-09 15:22:42 +0530

    Verify the page title by switching between the tabs

---
+++ Object Repository/Epic-2063-My Request Listing Page/DDS-1699/Verify the page title by switching between the tabs.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Verify the page title by switching between the tabs</name>
+   <tag></tag>
+   <elementGuidId>8cc86bdf-b80a-44d5-9782-f54f31e38fc4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>html/body/dew-app-root/dew-irequest-layout/irequest-header/dew-row[2]/dew-col[2]/dew-flex/dew-flex-item[1]/div/a
+</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
