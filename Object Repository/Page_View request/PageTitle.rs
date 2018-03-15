ae4b65650eabf1f8f0bfba4e12e770e653adfe6e
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-03-15 12:13:41 +0530

    Page Title TC

---
+++ Object Repository/Page_View request/PageTitle.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>PageTitle</name>
+   <tag></tag>
+   <elementGuidId>e149c0e8-66e8-4092-8ef2-dd1774e16c75</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='heading']/dew-flex/dew-flex-item[2]/h3</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
