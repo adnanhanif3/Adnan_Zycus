1acec864779a8a4404880ea9c6fc11d25bf38e54
pooja.wali <pooja.wali@ZIPLBLRSY102.zycus.net>
2018-03-22 18:32:53 +0530

    Draft object repo

---
+++ Object Repository/Page_Service request/Deselect filter 6.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Deselect filter 6</name>
+   <tag></tag>
+   <elementGuidId>058f7934-2cb8-4979-a137-3a264d6165cc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-col[@class=&quot;d-flex justify-content-start align-items-center ng-tns-c22-0 col&quot;]/span[1]/label</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
