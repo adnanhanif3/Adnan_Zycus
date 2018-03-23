3456f2ae53623fbb9b915d4caa76e7337958f63f
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-03-23 13:25:57 +0530

    Adding test cases.

---
+++ Object Repository/Page_ReportAnIssuePopUp/CloseButton.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>CloseButton</name>
+   <tag></tag>
+   <elementGuidId>43598bc4-81ea-451e-9249-0c0a9aa11d91</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//dew-icon[@class='undefined icon icon-close']</value>
+   </webElementProperties>
+</WebElementEntity>
