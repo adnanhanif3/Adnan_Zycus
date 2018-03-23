f50970ac79466862e33a7f1418c72523ad9af987
pooja.wali <pooja.wali@ZIPLBLRSY102.zycus.net>
2018-03-23 15:54:30 +0530

    error handing object repo

---
+++ Object Repository/Home page error handling repo/Error2.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Error2</name>
+   <tag></tag>
+   <elementGuidId>539d3416-404e-4d4e-a310-ea34b0b6209f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>html/body/dew-app-root/div/div[2]/button</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
