338f6422edf86072114b4f341095919493410e7d
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-03-23 19:34:07 +0530

    Print Action

---
+++ Object Repository/Page_View request/Print Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Print Button</name>
+   <tag></tag>
+   <elementGuidId>cca5bf7b-3b9d-4d19-b80c-447ac51001f5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-flex-item[@class='d-block']/a[text()='Print']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
