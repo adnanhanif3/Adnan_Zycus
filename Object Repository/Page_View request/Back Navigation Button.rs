3d2f473e9a151c8c8052c1250d1a3f0f6e40c571
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-03-23 17:42:09 +0530

    Back Navigation Button

--- Object Repository/Page_View request/Back Navigation Button.rs
+++ Object Repository/Page_View request/Back Navigation Button.rs
@@ -1,16 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>Error2</name>
+   <name>Back Navigation Button</name>
    <tag></tag>
-   <elementGuidId>539d3416-404e-4d4e-a310-ea34b0b6209f</elementGuidId>
+   <elementGuidId>7088b91a-6d0d-4360-976f-591714c757cf</elementGuidId>
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>html/body/dew-app-root/div/div/button</value>
+         <value>//dew-flex-item[@class=&quot;d-block&quot;]/h3/dew-icon</value>
       </entry>
       <entry>
          <key>BASIC</key>
+         <value></value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
