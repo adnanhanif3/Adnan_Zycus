47ce38931a5947732941f1d70dec7e8736f09cb9
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-03-26 20:04:24 +0530

    Save As PDF

---
+++ Object Repository/Page_View request/Save As PDF button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Save As PDF button</name>
+   <tag></tag>
+   <elementGuidId>e7cafac8-6981-4fca-bd09-8a9842a69edb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-flex-item[@class='d-block']/a[text()='Save as PDF']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
