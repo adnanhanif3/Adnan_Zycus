0cc94c2071c9c2d6dd827f3fc94119d574ed6dc8
priyanka.n <priyanka.n@ZIPLBLRSY118.zycus.net>
2018-03-26 10:44:59 +0530

    New changes

---
+++ Object Repository/Page_Home/Menu Nav/Completed.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Completed</name>
+   <tag></tag>
+   <elementGuidId>bf6ec37b-28f4-4536-b431-0fb6a5a67353</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>.//*[@id='filterModal']/div/div/div[2]/dew-row[2]/dew-col/div/span[5]/label</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
