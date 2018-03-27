35f540bc552b018506621354103817f332f80c3c
pooja.wali <pooja.wali@ZIPLBLRSY102.zycus.net>
2018-03-27 15:41:25 +0530

    Adding TC

---
+++ Object Repository/Page_Approval Listing/Approval_dropdwn.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Approval_dropdwn</name>
+   <tag></tag>
+   <elementGuidId>34b88c97-825a-4f0f-90ca-31acf9d1d3de</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-flex-item[2]/div/div/a</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Page_Approval Listing/TODO_tab.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>TODO_tab</name>
+   <tag></tag>
+   <elementGuidId>8496f2b6-120e-49aa-b0a7-47b28b189a69</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-irequest-layout/irequest-header/dew-row[2]/dew-col[2]/dew-flex/dew-flex-item[2]/div/a</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
