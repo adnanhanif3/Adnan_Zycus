2af6293f92337dda34f2f363c9ad4266226fd48a
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-03-27 20:22:54 +0530

    Preview WorkFlow TC

---
+++ Object Repository/Page_View request/First Actor Designation.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>First Actor Designation</name>
+   <tag></tag>
+   <elementGuidId>b5c43b35-79a8-406d-ae71-e789e875cc8e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>.//dew-text[text()='Requester']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
