db42b1af62c5535c63434849e0342acaf77b0de6
vinay.patel <vinay.patel@ZIPLBLRSY111.zycus.net>
2018-03-28 13:50:51 +0530

    Modified 1723_02 TODO's Navigation to homepage

--- Scripts/2067/1723/Verify the navigation to homepage/Script1522064862108.groovy
+++ Scripts/2067/1723/Verify the navigation to homepage/Script1522064862108.groovy
@@ -23,17 +23,9 @@ WebUI.callTestCase(findTestCase('Common TC/Login To irequest portal'), [:])
 
 WebUI.click(findTestObject('Page_Approval Listing/Pending_approvals'))
 
-WebUI.click(findTestObject('Home page error handling repo/Error2'))
-
-WebUI.click(findTestObject('Home page error handling repo/Error1'))
-
-WebUI.delay(1)
-
 WebUI.click(findTestObject('Page_Approval Listing/Btn_review'))
 
 WebUI.verifyElementPresent(findTestObject('Page_Approval Listing/Home_breadcrumb'), 0)
-
-WebUI.delay(1)
 
 WebUI.click(findTestObject('Page_Approval Listing/Home_breadcrumb'))
 
