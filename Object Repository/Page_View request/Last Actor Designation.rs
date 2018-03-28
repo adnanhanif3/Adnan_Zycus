609371f5ae388bcccafdd1cd016db9afab00245e
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-03-28 13:16:49 +0530

    Preview Workflow TC

---
+++ Object Repository/Page_View request/Last Actor Designation.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Last Actor Designation</name>
+   <tag></tag>
+   <elementGuidId>021a5173-7b03-40c3-afb5-3ab1348ce210</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[text()='Request Manager']/following-sibling::div//dew-text[text()='Request Manager']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
