e8821b51e7e295b5240d2c0186247c697bf1d15c
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-03-30 16:20:52 +0530

    Adding smoke suite

---
+++ Test Suites/SmokeSuite.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SmokeSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-30T16:07:16</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f5002177-25ba-4b7e-875a-b13a71fa91a4</testSuiteGuid>
+   <testCaseLink>
+      <guid>beb8fe0a-c06c-4511-bd83-a2bf8795875c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2051/1886/Verify the top menu options in iRequest homepage</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>309f5616-bef8-4f06-b60c-73c9764aa605</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2065/1878/TC 1878_01_submit(Smoke test)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38599d0f-afe9-44ea-81ee-47e8e8c62303</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2063/1698/Verify the request listing page-Smoke Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ee325147-13f6-43f3-b91b-ea3ac75c9027</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2069/1714/Verify Basic Details for View Request Page(Smoke-View Request)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8f614613-df2a-4fec-9eb2-a1f7e8ffa211</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2064/1694/TC1694_01_SubmitRequestWithBasicDetails(Smoke-CreateRequest)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
