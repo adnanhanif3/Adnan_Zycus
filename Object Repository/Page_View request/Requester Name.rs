01cad403884ea0ca2b87666a0f3359cb3a6fbb5d
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-04-02 21:05:48 +0530

    Preview Workflow

---
+++ Object Repository/Page_View request/Requester Name.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Requester Name</name>
+   <tag></tag>
+   <elementGuidId>f879453a-e570-4d39-a562-728af1bafdbc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-text[text()='Requester']/following-sibling::dew-text</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Page_View request/User Name.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>User Name</name>
+   <tag></tag>
+   <elementGuidId>d88881eb-81ad-4310-92a3-a26832c41254</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@class=&quot;btn-link nav-menu&quot;]/span</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
