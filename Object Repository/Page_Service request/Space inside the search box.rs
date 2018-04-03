cf1abff9b08a21510b1e12380efbe57bad5647b0
priyanka.n <priyanka.n@ZIPLBLRSY118.zycus.net>
2018-04-03 17:02:24 +0530

    Recent changes

---
+++ Object Repository/Page_Service request/Space inside the search box.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Space inside the search box</name>
+   <tag></tag>
+   <elementGuidId>ae383eeb-2e69-43ed-a50a-dd411369bf92</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>html/body/dew-app-root/dew-irequest-layout/dew-block/dew-my-requests/dew-tabset/div/dew-tab/dew-block/dew-row[1]/dew-col[2]/div/input</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Page_View request/iRequets portal.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>iRequets portal</name>
+   <tag></tag>
+   <elementGuidId>bb364483-3780-416f-bfa6-d5f769856415</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>html/body/dew-app-root/dew-irequest-layout/dew-block/ng-component/dew-tabset/div/dew-tab[1]/dew-block/dew-scroll-section[2]/dew-container/dew-paper/dew-section</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
