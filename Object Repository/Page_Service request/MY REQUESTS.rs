2600b8d244b300452a6e9ed2ed63df7e2c99f98d
priyanka.n <priyanka.n@ZIPLBLRSY118.zycus.net>
2018-04-16 17:53:09 +0530

    New

---
+++ Object Repository/Page_Service request/MY REQUESTS.rs
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>MY REQUESTS</name>
+   <tag></tag>
+   <elementGuidId>4248b0ef-5043-4edb-ad7e-0facd0215b26</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//dew-col[@class='col-8']/dew-flex/dew-flex-item[3]/div/a</value>
+   </webElementProperties>
+</WebElementEntity>
