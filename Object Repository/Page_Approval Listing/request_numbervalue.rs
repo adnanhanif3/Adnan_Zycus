726363a7001ca95f497370fc4ba9b9db0aed50ba
pooja.wali <pooja.wali@ZIPLBLRSY102.zycus.net>
2018-04-16 18:01:27 +0530

    Revert "Revert "Adding the To DO's test cases""
    This reverts commit 4e7b076e755924cd668c2c212e8456071cd38daa.

---
+++ Object Repository/Page_Approval Listing/request_numbervalue.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>request_numbervalue</name>
+   <tag></tag>
+   <elementGuidId>497913bb-7a35-4c0d-ab7c-50eb5592919c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-block[@class=&quot;p-4 d-block position-relative clearfix&quot;]/dew-approvals-card[1]/dew-paper/div/div[1]/dew-row/dew-col[1]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
