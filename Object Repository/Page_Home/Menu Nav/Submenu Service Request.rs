d4542f2256af5ec99f2e3687661a1fb528053670
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-04-16 15:46:32 +0530

    My request and service request xpaths

--- Object Repository/Page_Home/Menu Nav/Submenu Service Request.rs
+++ Object Repository/Page_Home/Menu Nav/Submenu Service Request.rs
@@ -6,16 +6,21 @@
    <elementGuidId>dca6c163-87f2-4406-b887-1d9a11c96819</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Service Requests')]</value>
+      </entry>
+      <entry>
          <key>BASIC</key>
+         <value>//a[contains(text(),'Service Requests')]</value>
       </entry>
    </selectorCollection>
-   <selectorMethod>BASIC</selectorMethod>
+   <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>//dew-col[@class='col-8']/dew-flex/dew-flex-item[3]/div/div/a</value>
+      <value>//a[contains(text(),'Service Requests')]</value>
    </webElementProperties>
 </WebElementEntity>
