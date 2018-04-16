454498fb64abb4de6abf066c207bba2337b5fb6b
pooja.wali <pooja.wali@ZIPLBLRSY102.zycus.net>
2018-04-16 19:52:25 +0530

    Adding TC

---
+++ Object Repository/Page_Home/Top Nav/Dropdown_create new btn.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Dropdown_create new btn</name>
+   <tag></tag>
+   <elementGuidId>4884d1f7-4a44-47bb-a08d-c5a045062efd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//span[2][text()='Create New']</value>
+   </webElementProperties>
+</WebElementEntity>
