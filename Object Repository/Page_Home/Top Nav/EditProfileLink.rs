497776f80823302723445371027769cb68c0378a
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-04-16 15:04:19 +0530

    Updating test case

--- Object Repository/Page_Home/Top Nav/EditProfileLink.rs
+++ Object Repository/Page_Home/Top Nav/EditProfileLink.rs
@@ -7,6 +7,7 @@
    <selectorCollection>
       <entry>
          <key>BASIC</key>
+         <value>//dew-row[@class='header-outlay1 row no-gutters']//dew-row/dew-col[3]/dew-flex/dew-flex-item/div/div/a[1]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -16,6 +17,6 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>//dew-row[@class='header-outlay1 row no-gutters']/dew-col[3]/dew-flex/dew-flex-item/div/div/a[1]</value>
+      <value>//dew-row[@class='header-outlay1 row no-gutters']//dew-row/dew-col[3]/dew-flex/dew-flex-item/div/div/a[1]</value>
    </webElementProperties>
 </WebElementEntity>
--- Scripts/2051/1668/Check for the avilablity of Edit Profile submenu/Script1520862574997.groovy
+++ Scripts/2051/1668/Check for the avilablity of Edit Profile submenu/Script1520862574997.groovy
@@ -21,7 +21,7 @@ import internal.GlobalVariable as GlobalVariable
 
 WebUI.callTestCase(findTestCase('Common TC/Ronaldo Login'), [:], FailureHandling.STOP_ON_FAILURE)
 
-WebUI.mouseOver(findTestObject('Page_Home/Top Nav/UserDisplayName'))
+WebUI.click(findTestObject('Page_Home/Top Nav/UserDisplayName'))
 
 WebUI.verifyElementPresent(findTestObject('Page_Home/Top Nav/EditProfileLink'), 0)
 
