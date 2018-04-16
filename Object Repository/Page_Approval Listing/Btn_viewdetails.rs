d133ef953d8826747744698217cf458ad8ffb4b6
pooja.wali <pooja.wali@ZIPLBLRSY102.zycus.net>
2018-04-16 18:29:17 +0530

    Adding To do's TC

---
+++ Object Repository/Page_Approval Listing/Btn_viewdetails.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Btn_viewdetails</name>
+   <tag></tag>
+   <elementGuidId>4460e174-8c16-4f35-9e93-00e6aeb191ee</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[text()='View Details']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
