96e2a53b1f2e2197e53605bd3473033d13892bd3
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-04-17 17:49:19 +0530

    Reject TC

---
+++ Object Repository/Page_View request/Pop up Approval comments.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Pop up Approval comments</name>
+   <tag></tag>
+   <elementGuidId>bf820972-a6c9-4ddb-b551-76473386a7dc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-modal[@ng-reflect-title=&quot;Comment for approval?&quot;]/descendant::textarea</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
