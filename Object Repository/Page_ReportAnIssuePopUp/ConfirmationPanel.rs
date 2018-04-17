a3c88dec653d2b223b6931f767e49b71204946f1
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-04-17 18:16:27 +0530

    New page element

---
+++ Object Repository/Page_ReportAnIssuePopUp/ConfirmationPanel.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ConfirmationPanel</name>
+   <tag></tag>
+   <elementGuidId>66281400-d717-4bdd-9b50-cc3216193e94</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='alert-panel d-flex justify-content-between']</value>
+   </webElementProperties>
+</WebElementEntity>
