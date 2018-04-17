f545e7e1fed86ecaae9aeb2011acf3d8fcec1da1
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-04-17 17:38:36 +0530

    Reject TC

---
+++ Object Repository/Page_View request/Pop up Reject Button.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Pop up Reject Button</name>
+   <tag></tag>
+   <elementGuidId>f5801aa3-de16-48c4-ad45-268eae9ce096</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@class='btn btn-success']/span/span[text()='Reject']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
