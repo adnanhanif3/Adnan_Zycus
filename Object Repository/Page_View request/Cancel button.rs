c906d3acb114a468d6a4bf5f48f1dc027740bc0e
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-04-17 18:05:10 +0530

    Cancel TC

---
+++ Object Repository/Page_View request/Cancel button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Cancel button</name>
+   <tag></tag>
+   <elementGuidId>8a87df44-d796-4e29-a680-06a190f70a9a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-modal[@class=&quot;modal fade show&quot;]/descendant::dew-icon</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
