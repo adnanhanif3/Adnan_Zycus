bd1f37e8d0dd1453131ae9fa81ef406698d4ffb6
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-04-17 17:30:28 +0530

    Approval TC

---
+++ Object Repository/Page_View request/WF ApprovalConfirmation.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>WF ApprovalConfirmation</name>
+   <tag></tag>
+   <elementGuidId>746a1b4d-64e9-41c0-bad7-8a828b9ee6ce</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(text(),'Request has been Approved')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
