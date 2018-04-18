f0e185f461668b0309ab9528ce275c39b63d8e48
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-04-18 15:30:06 +0530

    New test cases added.

---
+++ Object Repository/Page_ReportAnIssuePopUp/AttachmnentError.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>AttachmnentError</name>
+   <tag></tag>
+   <elementGuidId>2d1ba26c-d1d9-4302-8605-4ab7e0e44220</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='alert alert-danger ng-trigger ng-trigger-fadeInAnimation']</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Scripts/2051/1889/Verify attaching a file with less than 5MB/Script1524044738402.groovy
@@ -0,0 +1,61 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('Common TC/Ronaldo Login'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.mouseOver(findTestObject('Page_Home/Menu Nav/HELP DESK'))
+
+WebUI.click(findTestObject('Page_Home/Menu Nav/ReportAnIssue'))
+
+WebUI.click(findTestObject('Page_ReportAnIssuePopUp/Module'))
+
+WebUI.click(findTestObject('Page_ReportAnIssuePopUp/Module-Approvals'))
+
+WebUI.click(findTestObject('Page_ReportAnIssuePopUp/MessageType'))
+
+WebUI.click(findTestObject('Page_ReportAnIssuePopUp/MessageType_FunctionalityIssue'))
+
+WebUI.click(findTestObject('Page_ReportAnIssuePopUp/Priority'))
+
+WebUI.click(findTestObject('Page_ReportAnIssuePopUp/Priority_High'))
+
+WebUI.setText(findTestObject('Page_ReportAnIssuePopUp/Summary'), 'Test')
+
+WebUI.setText(findTestObject('Page_ReportAnIssuePopUp/Description'), 'Test')
+
+String userDir = System.getProperty('user.dir')
+
+String filePath = (userDir + '\\') + 'Data Files\\App Test Data\\FilesToUpload\\chapter2- 2+.pdf'
+
+WebUI.uploadFile(findTestObject('Page_ReportAnIssuePopUp/AttachmentField'), filePath)
+
+WebUI.delay(5)
+
+WebUI.click(findTestObject('Page_ReportAnIssuePopUp/Submit'))
+
+WebUI.delay(15)
+
+Text = WebUI.getText(findTestObject('Page_ReportAnIssuePopUp/ConfirmationPanel'))
+
+WebUI.verifyMatch(Text, 'Issue Submitted', false)
+
+WebUI.closeBrowser()
+
