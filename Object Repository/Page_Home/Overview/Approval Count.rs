9a12fd2b423802276ea8c5770486c5f8c6bc6222
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-04-19 15:19:44 +0530

    Added test case and page element.

---
+++ Object Repository/Page_Home/Overview/Approval Count.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Approval Count</name>
+   <tag></tag>
+   <elementGuidId>3bea5d6b-e1ca-41fd-afe2-29b877848f8b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//dew-block[@class='d-block position-relative clearfix']/dew-irequest/dew-section[1]/dew-block/dew-block/dew-row/dew-col[1]/dew-card/dew-flex/dew-flex-item[2]/p[@class='font-weight-bold title mb-0']</value>
+   </webElementProperties>
+</WebElementEntity>
