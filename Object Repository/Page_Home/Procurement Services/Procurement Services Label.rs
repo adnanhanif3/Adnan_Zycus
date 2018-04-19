bd7384642f1d1daeb01a756d8c2e9264593e5cfe
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-04-19 15:57:41 +0530

    Adding new test case

---
+++ Object Repository/Page_Home/Procurement Services/Procurement Services Label.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Procurement Services Label</name>
+   <tag></tag>
+   <elementGuidId>3f95738c-5c15-4519-85c4-85127c684328</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//dew-block[@class='d-block position-relative clearfix']/dew-irequest/dew-section[2]/dew-flex/dew-flex-item/h5</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Page_Home/Procurement Services/ProcurementServicesList.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ProcurementServicesList</name>
+   <tag></tag>
+   <elementGuidId>ff5d1a6c-d651-4c82-b9c9-314e841f29ea</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//dew-block[@class='d-block position-relative clearfix']/dew-irequest/dew-section[2]</value>
+   </webElementProperties>
+</WebElementEntity>
