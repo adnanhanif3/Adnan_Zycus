026884b520afb6ae25e6eebe17f4dfc5789b0a46
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-04-20 11:23:48 +0530

    WF TC

---
+++ Object Repository/Page_View request/WF Approver Name.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>WF Approver Name</name>
+   <tag></tag>
+   <elementGuidId>a2230116-bbc8-4437-be57-6a63d6b08db3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-text[text()='Approver']/following-sibling::dew-text</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
