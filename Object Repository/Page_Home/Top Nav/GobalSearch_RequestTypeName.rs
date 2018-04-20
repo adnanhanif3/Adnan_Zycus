50e09fb86f6b32578ee4325dd083f4996e0f901f
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-04-20 10:48:21 +0530

    Added test case

---
+++ Object Repository/Page_Home/Top Nav/GobalSearch_RequestTypeName.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>GobalSearch_RequestTypeName</name>
+   <tag></tag>
+   <elementGuidId>4d84cf3f-d285-477e-8b2e-6e910525b9b4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//li[@class='text-muted']</value>
+   </webElementProperties>
+</WebElementEntity>
