da282bdb3dd0594d4d9adb4bbb783c01c800e7a6
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-05-07 15:19:13 +0530

    Adding  test case

---
+++ Object Repository/Page_Home/Banner/BannerSection.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>BannerSection</name>
+   <tag></tag>
+   <elementGuidId>c0ecf879-794a-4308-8d79-e061b1e31172</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//dew-carousel[@class='border dew-carousel']/dew-carousel-content</value>
+   </webElementProperties>
+</WebElementEntity>
