b04866a75fff47c6d2a601b1a56f61204294ceb8
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-05-07 11:14:33 +0530

    Adding new test case

---
+++ Object Repository/Page_Global Search/FirstResult.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>FirstResult</name>
+   <tag></tag>
+   <elementGuidId>7e492b2c-a1f4-49ba-b268-b951cce5ae70</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='search-result-container py-3']/ul/li</value>
+   </webElementProperties>
+</WebElementEntity>
