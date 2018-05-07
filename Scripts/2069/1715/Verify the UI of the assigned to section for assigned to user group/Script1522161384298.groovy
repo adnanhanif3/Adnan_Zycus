3fcccd566167c98ae5bb65b242495a56c9a1e75a
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-05-07 20:45:10 +0530

    commit

--- Scripts/2069/1715/Verify the UI of the assigned to section for assigned to user group/Script1522161384298.groovy
+++ Scripts/2069/1715/Verify the UI of the assigned to section for assigned to user group/Script1522161384298.groovy
@@ -21,6 +21,8 @@ import internal.GlobalVariable as GlobalVariable
 
 WebUI.callTestCase(findTestCase('Common TC/Ronaldo Login'), [:], FailureHandling.STOP_ON_FAILURE)
 
+WebUI.maximizeWindow()
+
 WebUI.acceptAlert()
 
 WebUI.sendKeys(findTestObject('Page_Home/Top Nav/Globalsearch_box'), 'Test email Notification')
