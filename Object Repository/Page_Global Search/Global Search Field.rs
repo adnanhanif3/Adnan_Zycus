4350a47cd4e02520157bde8cb6a8cd06af58ccd2
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-05-07 10:59:59 +0530

    Adding new test case

---
+++ Object Repository/Page_Global Search/Global Search Field.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Global Search Field</name>
+   <tag></tag>
+   <elementGuidId>b32c538b-6527-4208-94f9-cb56cbe4ddba</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='input-group']/input</value>
+   </webElementProperties>
+</WebElementEntity>
