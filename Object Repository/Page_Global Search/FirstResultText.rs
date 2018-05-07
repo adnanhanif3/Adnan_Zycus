657b1c6b6a8b1a0c97ae78b22348797d25e1d25e
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-05-07 11:42:48 +0530

    Adding new test case

---
+++ Object Repository/Page_Global Search/FirstResultText.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>FirstResultText</name>
+   <tag></tag>
+   <elementGuidId>068a47a6-30e3-4d0f-981e-97506abb8ad0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='search-result-container py-3']/ul/li/span</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Page_Global Search/SecondResult.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>SecondResult</name>
+   <tag></tag>
+   <elementGuidId>61519212-38e5-4a67-8953-568ed00cb19b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='search-result-container py-3']/ul/li[2]</value>
+   </webElementProperties>
+</WebElementEntity>
