ec948f2200511a134a4724a7463701dc7cc782c5
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-05-07 12:08:22 +0530

    Adding test case

---
+++ Object Repository/Page_Global Search/FirstTextBold.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>FirstTextBold</name>
+   <tag></tag>
+   <elementGuidId>d71752ef-31a1-476c-af12-5c9561e45ce3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='search-result-container py-3']/ul/li[1]/span[1]/span</value>
+   </webElementProperties>
+</WebElementEntity>
