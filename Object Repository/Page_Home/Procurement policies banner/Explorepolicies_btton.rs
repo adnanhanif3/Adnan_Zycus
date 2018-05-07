b35a936ef11a3e80988551072e5112085dfd3690
pooja.wali <pooja.wali@ZIPLBLRSY102.zycus.net>
2018-05-07 12:32:26 +0530

    Adding DDS-1685 test cases

---
+++ Object Repository/Page_Home/Procurement policies banner/Explorepolicies_btton.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Explorepolicies_btton</name>
+   <tag></tag>
+   <elementGuidId>f652a722-0ad1-4dfb-93c3-8324ee9efbdb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;dew-container policies-bg&quot;]/div/dew-btn/button</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
