cd2f5aff0aebc64ae367144d9ca07c9a4dbceac9
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-05-07 12:01:35 +0530

    Adding new test case

---
+++ Object Repository/Page_Global Search/CreateNew.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>CreateNew</name>
+   <tag></tag>
+   <elementGuidId>4c8f9fab-1edb-4a1d-831d-dc84dccdf223</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='search-result-container py-3']/ul/li[1]/span[2]</value>
+   </webElementProperties>
+</WebElementEntity>
