be12672629eba5c1e7f7711578fec8a27dfdb522
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-05-09 18:38:02 +0530

    helpdesk

---
+++ Object Repository/Page_Home/Menu Nav/HelpDesk.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>HelpDesk</name>
+   <tag></tag>
+   <elementGuidId>2e731357-8162-4352-bf79-1d7e679020d2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//dew-col[@class='col-12']/dew-flex/dew-flex-item[4]/div/a</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[contains(text(),'Help Desk')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//dew-col[@class='col-12']/dew-flex/dew-flex-item[4]/div/a</value>
+   </webElementProperties>
+</WebElementEntity>
