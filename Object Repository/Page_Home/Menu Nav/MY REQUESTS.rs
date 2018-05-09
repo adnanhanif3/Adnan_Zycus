75e917aa2698de89fbe424ce3f37e9874c2ea6a4
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-05-09 15:47:10 +0530

    Updating test case

--- Object Repository/Page_Home/Menu Nav/MY REQUESTS.rs
+++ Object Repository/Page_Home/Menu Nav/MY REQUESTS.rs
@@ -6,14 +6,21 @@
    <elementGuidId>d43640a0-66ac-49ed-97d3-d1319a4eba8d</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>BASIC</key>
-         <value>//dew-col[@class='col-8']/dew-flex/dew-flex-item[3]/div/a</value>
+         <key>XPATH</key>
+         <value></value>
       </entry>
       <entry>
-         <key>XPATH</key>
-         <value>//dew-col[3]/div/span</value>
+         <key>BASIC</key>
+         <value></value>
       </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//dew-block[@class='p-0 dropdown-navs-header for-large-screens dew-block']/div[@class='dew-container']/dew-row[@class='justify-content-center row']/dew-col[3]/div</value>
+   </webElementProperties>
 </WebElementEntity>
--- Object Repository/Page_Home/Menu Nav/Submenu Service Request.rs
+++ Object Repository/Page_Home/Menu Nav/Submenu Service Request.rs
@@ -7,20 +7,20 @@
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>//a[contains(text(),'Service Requests')]</value>
+         <value></value>
       </entry>
       <entry>
          <key>BASIC</key>
-         <value>//a[contains(text(),'Service Requests')]</value>
+         <value>//dew-block[@class='p-0 dropdown-navs-header for-large-screens dew-block']/div[@class='dew-container']/dew-row[@class='justify-content-center row']/dew-col[3]/div/div/a</value>
       </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>//a[contains(text(),'Service Requests')]</value>
+      <value>//dew-block[@class='p-0 dropdown-navs-header for-large-screens dew-block']/div[@class='dew-container']/dew-row[@class='justify-content-center row']/dew-col[3]/div/div/a</value>
    </webElementProperties>
 </WebElementEntity>
