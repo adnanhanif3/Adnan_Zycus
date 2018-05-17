45a298b43d3309625f84a9f173b447e3e43a5f9f
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-05-17 19:33:10 +0530

    Commit FAQ

--- Object Repository/FAQs Home and Listing Page/Have more questions-READ FAQs.rs
+++ Object Repository/FAQs Home and Listing Page/Have more questions-READ FAQs.rs
@@ -6,12 +6,11 @@
    <elementGuidId>27de2994-dd28-457e-af38-ca4d0280d43f</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>BASIC</key>
-         <value></value>
+         <key>XPATH</key>
+         <value>//span[text()='Read FAQs']</value>
       </entry>
       <entry>
-         <key>XPATH</key>
-         <value>//dew-section/div/dew-section-body/div</value>
+         <key>BASIC</key>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
