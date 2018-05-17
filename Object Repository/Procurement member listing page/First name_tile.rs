de6ddf5f7d1183724ef98b6ba6b97e305722d28e
pooja.wali <pooja.wali@ZIPLBLRSY102.zycus.net>
2018-05-17 19:17:44 +0530

    Adding 1686 test cases

---
+++ Object Repository/Procurement member listing page/First name_tile.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>First name_tile</name>
+   <tag></tag>
+   <elementGuidId>92974f66-ee63-4b6b-98ac-2d345bb5c151</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-card[@class=&quot;team-cards has-shadow card&quot;]/dew-card-body/div/div/p</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Procurement member listing page/Home_procurement team.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Home_procurement team</name>
+   <tag></tag>
+   <elementGuidId>023c8b7e-3b97-4408-87ab-895ee3826019</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;dew-container&quot;]/dew-section/dew-flex/dew-flex-item[1]/div</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
