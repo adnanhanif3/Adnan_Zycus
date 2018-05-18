bb0fc82ecb25c9ae327d7fc52a7c08c4cfdfac9f
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-05-18 12:00:59 +0530

    WF updated tc

--- Scripts/2069/1717/Verify the status if the past node is Approval Actor and Reject Action has been taken/Script1524204991722.groovy
+++ Scripts/2069/1717/Verify the status if the past node is Approval Actor and Reject Action has been taken/Script1524204991722.groovy
@@ -23,6 +23,8 @@ WebUI.callTestCase(findTestCase('Common TC/Login To irequest portal'), [:])
 
 WebUI.maximizeWindow()
 
+WebUI.waitForPageLoad(10)
+
 WebUI.click(findTestObject('Page_Service request/Requests list on homepage'))
 
 WebUI.delay(4)
@@ -40,6 +42,10 @@ WebUI.click(findTestObject('Page_Service request/Draft deselect'))
 WebUI.delay(5)
 
 WebUI.click(findTestObject('Page_Service request/btn_ViewRequest1'))
+
+WebUI.delay(2)
+
+WebUI.scrollToElement(findTestObject('Page_View request/WF Status Approver'), 4)
 
 WebUI.verifyElementText(findTestObject('Page_View request/WF Status Approver'), 'Rejected')
 
