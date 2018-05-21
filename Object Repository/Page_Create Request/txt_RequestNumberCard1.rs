2b95189141aa08826d077475baa5eb1c7ed08b2d
vinay.patel <vinay.patel@ZIPLBLRSY111.zycus.net>
2018-05-21 18:44:21 +0530

    Fixed 1694_03

--- Object Repository/Page_Create Request/txt_RequestNumberCard1.rs
+++ Object Repository/Page_Create Request/txt_RequestNumberCard1.rs
@@ -6,12 +6,11 @@
    <elementGuidId>1faef021-fcb2-48e7-beb0-fae33037b3be</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//dew-block/div[2]/div[1]/dew-service-request-card/dew-paper/div/div[1]/dew-row/dew-col[1]/p/span</value>
+         <key>BASIC</key>
       </entry>
       <entry>
-         <key>BASIC</key>
-         <value></value>
+         <key>XPATH</key>
+         <value>//div/div[2]/div/dew-tab/div[2]/div[1]/dew-service-request-card/div/div[1]/dew-row/dew-col[1]/p/span</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
