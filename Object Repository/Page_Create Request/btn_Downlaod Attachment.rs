fe5f41419d1f613d35980e7dda4ddd709a76f066
vinay.patel <vinay.patel@ZIPLBLRSY111.zycus.net>
2018-05-21 20:00:49 +0530

    Added TC1690_09

---
+++ Object Repository/Page_Create Request/btn_Downlaod Attachment.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Downlaod Attachment</name>
+   <tag></tag>
+   <elementGuidId>83152bc7-8b14-4b82-b966-40b04ef8b6eb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[text()='Download']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
