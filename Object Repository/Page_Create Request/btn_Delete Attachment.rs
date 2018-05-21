516bf4ed6b66ea004af9281d208572a627625b14
vinay.patel <vinay.patel@ZIPLBLRSY111.zycus.net>
2018-05-21 19:54:23 +0530

    Added TC1690_08

---
+++ Object Repository/Page_Create Request/btn_Delete Attachment.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Delete Attachment</name>
+   <tag></tag>
+   <elementGuidId>4e3624fe-171f-49a0-a62d-84dfe6cdf0af</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[text()='Download']//following-sibling::img</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
