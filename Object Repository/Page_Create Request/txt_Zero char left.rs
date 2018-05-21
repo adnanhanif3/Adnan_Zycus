0ed37ab5ac5446421488a7717f9b5a3220e4846e
vinay.patel <vinay.patel@ZIPLBLRSY111.zycus.net>
2018-05-21 20:18:10 +0530

    Adding TC1690_10

---
+++ Object Repository/Page_Create Request/txt_Zero char left.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>txt_Zero char left</name>
+   <tag></tag>
+   <elementGuidId>cee0f58e-593b-4ddd-832f-e7510d46de16</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[text()=&quot;0 Char left&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
