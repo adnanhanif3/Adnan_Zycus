e9eea251c004d69616c6897b12f304eac5a7223e
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-05-21 11:54:14 +0530

    Adding new test case

---
+++ Object Repository/Page_ReportAnIssuePopUp/UploadCancel.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>UploadCancel</name>
+   <tag></tag>
+   <elementGuidId>913a9d23-cd38-4481-9000-41623981dfe0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='modal-body']/dew-modal-body/div/div/div/p/span/dew-icon</value>
+   </webElementProperties>
+</WebElementEntity>
