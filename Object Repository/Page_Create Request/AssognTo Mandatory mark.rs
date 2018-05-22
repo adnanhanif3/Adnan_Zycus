839fcc4f198720e0ca186edfd4ec7e7f27a07a82
vinay.patel <vinay.patel@ZIPLBLRSY111.zycus.net>
2018-05-22 16:27:18 +0530

    Adding TC1691_07

---
+++ Object Repository/Page_Create Request/AssognTo Mandatory mark.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>AssognTo Mandatory mark</name>
+   <tag></tag>
+   <elementGuidId>f937ef6f-3082-45cd-ad0e-ca7a25132d63</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div/irequest-new-request/div[2]/form/dew-block[2]/irequest-new-assign/dew-row/dew-col/div/label/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
