366d3baf8d587c025fa68ab941938f3b5f68ef9e
vinay.patel <vinay.patel@ZIPLBLRSY111.zycus.net>
2018-05-23 17:21:37 +0530

    Adding TC to 1728

---
+++ Object Repository/Page_Service request/btn_Close popup Approve.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Close popup Approve</name>
+   <tag></tag>
+   <elementGuidId>d4ffac69-5132-4d8e-bb10-c9e20656b6fd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-block/dew-approvals-card[1]/dew-card/dew-card-body/dew-modal[2]/div/div/button</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Page_Service request/btn_Close popup Reject.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Close popup Reject</name>
+   <tag></tag>
+   <elementGuidId>37d86e04-10bd-4c78-87cf-17e049d5ee65</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-block/dew-approvals-card[1]/dew-card/dew-card-body/dew-modal[1]/div/div/button</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
