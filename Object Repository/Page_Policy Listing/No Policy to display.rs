4e2a6698b814bef5cf8b1c5c3cd525a823c3417a
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-05-24 19:56:08 +0530

    Policy TC

---
+++ Object Repository/Page_Policy Listing/No Policy to display.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>No Policy to display</name>
+   <tag></tag>
+   <elementGuidId>34452a63-8b31-4ea1-bd06-19364b661110</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[text()='All']/parent::div/following-sibling::div</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
