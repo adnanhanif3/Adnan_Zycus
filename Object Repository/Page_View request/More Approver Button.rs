273a8881d9daf20ede591ac99beb64728de0aff5
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-05-24 19:57:04 +0530

    common TC

---
+++ Object Repository/Page_View request/More Approver Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>More Approver Button</name>
+   <tag></tag>
+   <elementGuidId>eb565c51-7705-4f95-b8fb-d626e86024bf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-btn[contains(@ng-reflect-display,'approvers')]/button</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
