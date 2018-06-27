03b4858fbb65f3f53efcfa550edae5aa084d2f22
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-06-27 14:25:46 +0530

    commit

--- Object Repository/Page_ReportAnIssuePopUp/ConfirmationPanel.rs
+++ Object Repository/Page_ReportAnIssuePopUp/ConfirmationPanel.rs
@@ -6,15 +6,11 @@
    <elementGuidId>66281400-d717-4bdd-9b50-cc3216193e94</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//div[contains(text(),' Please fill the mandatory fields')]</value>
-      </entry>
-      <entry>
          <key>BASIC</key>
-         <value>//div[@class='alert-panel d-flex justify-content-between']</value>
+         <value></value>
       </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
--- Object Repository/Page_ReportAnIssuePopUp/Submit.rs
+++ Object Repository/Page_ReportAnIssuePopUp/Submit.rs
@@ -3,7 +3,7 @@
    <description></description>
    <name>Submit</name>
    <tag></tag>
-   <elementGuidId>ecf88cda-8db9-40b3-9b74-4d756526d1b8</elementGuidId>
+   <elementGuidId>c5915045-637b-41c5-b040-1db4dc30b6ee</elementGuidId>
    <selectorCollection>
       <entry>
          <key>XPATH</key>
@@ -11,20 +11,9 @@
       </entry>
       <entry>
          <key>BASIC</key>
-<<<<<<< HEAD
-         <value>//div[@class='modal-footer p-0 p-4']/dew-modal-footer/dew-btn</value>
-=======
-         <value>//dew-modal-footer[@class=&quot;modal-footer p-0&quot;]/dew-btn/button</value>
->>>>>>> branch 'master' of https://github.com/PrabodhGhosh/iRequestAutomation
+         <value></value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
-   <webElementProperties>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath</name>
-      <type>Main</type>
-      <value>//dew-modal-footer[@class=&quot;modal-footer p-0&quot;]/dew-btn/button</value>
-   </webElementProperties>
 </WebElementEntity>
