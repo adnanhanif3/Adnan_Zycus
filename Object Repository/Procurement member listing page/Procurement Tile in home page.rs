e70a3d7c8a954c0c31146097e21d7ff1feb1d125
vinay.patel <vinay.patel@ZIPLBLRSY111.zycus.net>
2018-06-27 18:47:25 +0530

    Updated TC_03 1686

---
+++ Object Repository/Procurement member listing page/Procurement Tile in home page.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Procurement Tile in home page</name>
+   <tag></tag>
+   <elementGuidId>40e46ced-24f9-41e5-90f0-88999510a676</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-section[@ng-reflect-heading=&quot;Procurement Team&quot;]/div/dew-section-body/dew-row/dew-col[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
