200f9c16d6a0fed6698846a7cf4dcfff3b32c1db
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-06-27 13:35:52 +0530

    commit

--- Object Repository/Page_ReportAnIssuePopUp/PopupHeader.rs
+++ Object Repository/Page_ReportAnIssuePopUp/PopupHeader.rs
@@ -6,11 +6,15 @@
    <elementGuidId>3f964fb1-1518-497f-9921-b31cd50f6862</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>XPATH</key>
+         <value>//h5[text()='Report an issue']</value>
+      </entry>
+      <entry>
          <key>BASIC</key>
          <value>//h5[@class='text-title w-100']</value>
       </entry>
    </selectorCollection>
-   <selectorMethod>BASIC</selectorMethod>
+   <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
