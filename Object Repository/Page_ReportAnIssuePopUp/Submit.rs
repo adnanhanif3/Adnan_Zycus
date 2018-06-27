1d2cac71e6b55859ce2eccfac391b189d53ea974
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-06-27 14:10:02 +0530

    commit

--- Object Repository/Page_ReportAnIssuePopUp/Submit.rs
+++ Object Repository/Page_ReportAnIssuePopUp/Submit.rs
@@ -6,19 +6,19 @@
    <elementGuidId>ecf88cda-8db9-40b3-9b74-4d756526d1b8</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//span[text()='Submit']/ancestor::button</value>
-      </entry>
-      <entry>
          <key>BASIC</key>
+<<<<<<< master
 <<<<<<< HEAD
          <value>//div[@class='modal-footer p-0 p-4']/dew-modal-footer/dew-btn</value>
 =======
          <value>//dew-modal-footer[@class=&quot;modal-footer p-0&quot;]/dew-btn/button</value>
 >>>>>>> branch 'master' of https://github.com/PrabodhGhosh/iRequestAutomation
+=======
+         <value></value>
+>>>>>>> 31a9aca commit
       </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
