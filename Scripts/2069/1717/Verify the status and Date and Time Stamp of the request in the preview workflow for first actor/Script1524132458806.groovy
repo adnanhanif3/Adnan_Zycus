0214703f4906fbacf76de062611f5416a1429769
adnan.hanif <adnan.hanif@ZIPLBLRSY119.zycus.net>
2018-06-28 11:45:48 +0530

    wf TC

---
+++ Scripts/2069/1717/Verify the status and Date and Time Stamp of the request in the preview workflow for first actor/Script1524132458806.groovy
@@ -0,0 +1,53 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('Common TC/Login To irequest portal'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.maximizeWindow()
+
+WebUI.sendKeys(findTestObject('Page_Home/Top Nav/Globalsearch_box'), 'Test email Notification')
+
+WebUI.click(findTestObject('Page_Home/Top Nav/Dropdown_create new btn'))
+
+WebUI.sendKeys(findTestObject('Page_Create Request/txt_RequestName'), 'Test_Status_Timestamp')
+
+WebUI.sendKeys(findTestObject('Page_Create Request/txtarea_Desc'), 'Testing status and timestamp of the requester')
+
+WebUI.click(findTestObject('Page_Create Request/btn_Submit'))
+
+WebUI.waitForElementPresent(findTestObject('Page_Create Request/icon_Close'), 20)
+
+WebUI.click(findTestObject('Page_Create Request/icon_Close'))
+
+WebUI.delay(5)
+
+String reqNumText = WebUI.getText(findTestObject('Page_Service request/Request_number'))
+
+WebUI.click(findTestObject('Page_Service request/View Request'))
+
+WebUI.delay(4)
+
+WebUI.verifyElementText(findTestObject('Page_View request/WF Status Requester'), 'Submitted')
+
+WebUI.delay(3)
+
+WebUI.closeBrowser()
+
