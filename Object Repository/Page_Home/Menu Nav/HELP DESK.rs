ee924fb4f6d5894ee9edaa1f9efab88426177e74
prabodh.ghosh <prabodh.ghosh@ZIPLBLRSY112.zycus.net>
2018-07-02 18:47:59 +0530

    Updating test case.

--- Object Repository/Page_Home/Menu Nav/HELP DESK.rs
+++ Object Repository/Page_Home/Menu Nav/HELP DESK.rs
@@ -7,7 +7,7 @@
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value>//dew-block[@class='p-0 dropdown-navs-header for-large-screens dew-block']/div[@class='dew-container']/dew-row[@class='justify-content-center row']/dew-col[4]/div</value>
+         <value>//dew-block[@class='p-0 dropdown-navs-header for-large-screens dew-block']/div[@class='dew-container']/dew-row[@class='justify-content-center row']/dew-col[3]/div</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -17,6 +17,6 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>//dew-block[@class='p-0 dropdown-navs-header for-large-screens dew-block']/div[@class='dew-container']/dew-row[@class='justify-content-center row']/dew-col[4]/div</value>
+      <value>//dew-block[@class='p-0 dropdown-navs-header for-large-screens dew-block']/div[@class='dew-container']/dew-row[@class='justify-content-center row']/dew-col[3]/div</value>
    </webElementProperties>
 </WebElementEntity>
