e6913e847512defcd07f8ee19ba246fa64296a25
vinay.patel <vinay.patel@ZIPLBLRSY111.zycus.net>
2018-07-03 12:09:30 +0530

    Reworked 2063/1707/Verify the action available for Returned request

--- Scripts/2063/1707/Verify the action avilable for Returned request/Script1520939716526.groovy
+++ Scripts/2063/1707/Verify the action avilable for Returned request/Script1520939716526.groovy
@@ -21,17 +21,25 @@ import internal.GlobalVariable as GlobalVariable
 
 WebUI.callTestCase(findTestCase('Common TC/Login To irequest portal'), [:], FailureHandling.STOP_ON_FAILURE)
 
-WebUI.mouseOver(findTestObject('Page_Home/Menu Nav/MY REQUESTS'))
+WebUI.click(findTestObject('Page_Service request/Requests list on homepage'))
 
-WebUI.click(findTestObject('Page_Service request/Service request xpath'), FailureHandling.STOP_ON_FAILURE)
+WebUI.delay(3)
+
+WebUI.click(findTestObject('Page_Service request/Inprogress deselect'))
+
+WebUI.click(findTestObject('Page_Service request/With RM deselect '))
+
+WebUI.click(findTestObject('Page_Service request/Awatting approval deselect '))
+
+WebUI.click(findTestObject('Page_Service request/Rejected deselect '))
+
+WebUI.click(findTestObject('Page_Service request/Draft deselect'))
 
 WebUI.delay(2)
 
-WebUI.verifyElementPresent(findTestObject('Page_Home/Menu Nav/Returned click'), 0)
+WebUI.verifyElementPresent(findTestObject('Page_Service request/Edit request button(Rejected, Returned)'), 0)
 
-WebUI.delay(2)
-
-WebUI.verifyElementPresent(findTestObject('Page_View request/Verify View'), 0)
+WebUI.verifyElementPresent(findTestObject('Page_Service request/View Request(Returned,Rejected)'), 0)
 
 WebUI.closeBrowser()
 
