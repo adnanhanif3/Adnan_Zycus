9c42245aa29327d47e7674798d44b28a646ebf2a
pooja.wali <pooja.wali@ZIPLBLRSY102.zycus.net>
2018-07-03 12:24:12 +0530

    commit

--- Scripts/2065/1874/TC_12_Attaching a file with 128MB/Script1526885725265.groovy
+++ Scripts/2065/1874/TC_12_Attaching a file with 128MB/Script1526885725265.groovy
@@ -19,7 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.callTestCase(findTestCase('Common TC/Ronaldo Login'), [:], FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('Common TC/Frank L Login'), [:], FailureHandling.STOP_ON_FAILURE)
 
 WebUI.sendKeys(findTestObject('Page_Home/Top Nav/Globalsearch_box'), 'Definition to test')
 
