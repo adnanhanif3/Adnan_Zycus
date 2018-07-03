fe12a3ba8eb1bf1a2a5acc0c6f0cbf8149004cb8
vinay.patel <vinay.patel@ZIPLBLRSY111.zycus.net>
2018-07-03 12:03:40 +0530

    Reworked 2063/1707/Action for Rejected request

---
+++ Object Repository/Page_Service request/View Request(Returned,Rejected).rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>View Request(Returned,Rejected)</name>
+   <tag></tag>
+   <elementGuidId>57f98e67-febd-45e5-9592-f2c36d8dc809</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//dew-tabset/div/div[2]/div/dew-tab/dew-block/div/div[1]/dew-service-request-card/dew-card/dew-card-body/dew-row/dew-col[4]/div/a[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
